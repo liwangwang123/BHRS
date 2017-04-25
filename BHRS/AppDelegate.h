8db1992f6e489566fb334a0ba92b183bd9597cb9
liwangwnag123 <2317790024@qq.com>
2017-04-25 16:00:11 +0800

    Initial Commit

---
+++ BHRS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BHRS
+//
+//  Created by 王力 on 2017/4/25.
+//  Copyright © 2017年 王力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BHRS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BHRS
+//
+//  Created by 王力 on 2017/4/25.
+//  Copyright © 2017年 王力. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
