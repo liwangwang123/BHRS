e6735b7f5bebec221e2b2b234ad2690a477318d1
wangli <1432103394@qq.com>
2017-05-04 12:58:09 +0800

    修改返回按钮  以及一些页面标题

--- BHRS/Mine/Controller/MineViewController.m
+++ BHRS/Mine/Controller/MineViewController.m
@@ -45,7 +45,7 @@
 - (void)viewWillAppear:(BOOL)animated{
   [super viewWillAppear:animated];
   self.navigationController.navigationBar.hidden = YES;
-    
+  self.tabBarController.tabBar.hidden = NO;
 
 }
 
